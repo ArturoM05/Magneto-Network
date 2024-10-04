f876a6202552642a2a9f8ce3cf99d973a43ca24d
Arturo Fernando Murgueytio Arias <afmurgueya@eafit.edu.co>
2024-10-03 19:57:14 -0500

    add basic visuals
    add basic visual to have an example

--- Magneto_Networkork_Site/settings.py
+++ Magneto_Networkork_Site/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+from django.contrib.messages import constants as message_constants
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,3 +97,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'debug',
+    message_constants.INFO: 'info',
+    message_constants.SUCCESS: 'success',
+    message_constants.WARNING: 'warning',
+    message_constants.ERROR: 'danger', 
+}
