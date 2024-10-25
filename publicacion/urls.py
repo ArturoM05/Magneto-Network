03c0b99a028d099762fee1bdd99ec4b464421391
Arturo Fernando Murgueytio Arias <afmurgueya@eafit.edu.co>
2024-10-25 09:49:56 -0500

    adding a realtime counter likes

--- publicacion/urls.py
+++ publicacion/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_algorithm
+from .views import home_algorithm, like_post
 
 urlpatterns = [
 			   path('',home_algorithm , name="home1"),
+               path('like_post/',like_post, name='like_post'),
 			   ]
--- publicacion/views.py
+++ publicacion/views.py
@@ -1,8 +1,10 @@
+import  json
 from django.shortcuts import render
 from cuenta.models import Usuario
 from .models import Publicacion
 from django.contrib import messages
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def home_algorithm(request):
     usuario_id = request.session.get('usuario_id')
@@ -13,16 +15,26 @@ def home_algorithm(request):
             publicacion = Publicacion(user=user.nombre,text=post_content, likes=0)
             publicacion.save()
             messages.info(request, 'tu post se ha publicado')
-        if 'like_post' in request.POST:
-            post_id = request.POST.get('like_post')
-            publicacion = Publicacion.objects(id=post_id).first()
-            publicacion.likes += 1
-            publicacion.save()
     publicaciones = list(Publicacion.objects())
     if user:
-        print(user.nombre)
         return render(request, 'home1.html', {'usuario': user, 'publicaciones': publicaciones})
     else:
         messages.error(request, 'tienes que iniciar sesion primero')
         return render(request, 'login.html')
+
+
+@csrf_exempt  # Considera la seguridad en tu implementación
+def like_post(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        post_id = data.get('post_id')
+
+        # Lógica para dar like a la publicación
+        publicacion = Publicacion.objects(id=post_id).first()
+        publicacion.likes += 1
+        publicacion.save()
+
+        return JsonResponse({'success': True, 'likes': publicacion.likes})
+
+    return JsonResponse({'success': False, 'error': 'Invalid request'}, status=400)
 
